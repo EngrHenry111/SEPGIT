f05ac1e843fe6b2dae326818b828fffee2d7b10a
EngrHenry111 <henryengrakpan@gmail.com>
2024-09-21 09:40:08 +0100

    git Collaboration

---
+++ Colab.js
@@ -0,0 +1,15 @@
+function displaymsg(logic){
+    setInterval(() => {
+        logic("Thank you for signing up ")
+       
+    }, 5000);
+
+    setInterval(() => {
+      
+       logic("please log in")
+       },  6000);
+}
+
+displaymsg((mag) =>{
+    console.log(mag)
+})
